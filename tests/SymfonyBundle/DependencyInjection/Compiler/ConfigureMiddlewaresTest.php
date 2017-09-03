f77241abc47dfdd733ac607c4b52fae316c33ea2
Cliff Odijk <cliff@jcid.nl>
2017-09-03 13:48:47 +0200

    Upgraded phpunit to allow 5.7 and 6.0

--- tests/SymfonyBundle/DependencyInjection/Compiler/ConfigureMiddlewaresTest.php
+++ tests/SymfonyBundle/DependencyInjection/Compiler/ConfigureMiddlewaresTest.php
@@ -6,7 +6,7 @@ use SimpleBus\SymfonyBridge\DependencyInjection\Compiler\ConfigureMiddlewares;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Definition;
 
-class ConfigureMiddlewaresTest extends \PHPUnit_Framework_TestCase
+class ConfigureMiddlewaresTest extends \PHPUnit\Framework\TestCase
 {
     /**
      * @var ContainerBuilder
